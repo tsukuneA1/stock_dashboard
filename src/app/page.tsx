7816600fc402255c773af5efd0eed198ecb1ed77
202420232022 <jinogame1210@gmail.com>
2025-04-16 00:29:57 +0900

    証券コード

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import yahooFinance from "yahoo-finance2";
 
+const TickerSymbol = "7974.T";
+
 export default async function Home() {
-  const quote = await yahooFinance.quote('7974.T');
+  const quote = await yahooFinance.quote(TickerSymbol);
 
   const junneriki = Number(quote.marketCap) / Number(quote.sharesOutstanding);
 
-  const summary = await yahooFinance.quoteSummary("7974.T", {
+  const summary = await yahooFinance.quoteSummary(TickerSymbol, {
     modules: ["summaryDetail", "financialData"],
   });
   
@@ -16,7 +18,7 @@ export default async function Home() {
 
   return (
     <main className="p-4">
-      <h1 className="text-xl font-bold mb-2">株式情報（7974.T）</h1>
+      <h1 className="text-xl font-bold mb-2">株式情報（{TickerSymbol}）</h1>
       <Card>
         <CardHeader>
           <CardTitle>株式情報</CardTitle>
